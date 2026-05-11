ee866f0336f4a8cad99b15fb3b307f01d4b24b76
t-ski <t.schiepanski@gmx.de>
2026-05-11 11:34:09 +0200

    Expose API (lib)

---
+++ src/api.lib.ts
@@ -0,0 +1,4 @@
+export { ModelContext } from "./ModelContext.js";
+export { ModelContextClient } from "./ModelContextClient.js";
+export { Registry } from "./Registry.js";
+export * from "./types.js";
